769b7ef5902dc998bee2420a8708f3498240186c
shaxzoddavronov <120119316+shaxzoddavronov@users.noreply.github.com>
2023-08-06 16:20:30 +0500

    Update titanic_app.py

--- titanic-surviver-classification/titanic_app.py
+++ titanic-surviver-classification/titanic_app.py
@@ -6,7 +6,7 @@ import seaborn as sns
 import base64
 import plotly.figure_factory as ff
 
-st.image('Titanic.png',width=500)
+st.image('titanic-surviver-classification/Titanic.png',width=500)
 st.title('Titanic Survival Prediction')
 
 st.write("""On April 15, 1912, during her maiden voyage, the widely considered “unsinkable”
